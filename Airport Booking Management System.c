ca5a6163d6d920b1d10e2b801ff9b00a6987f13c
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-05-25 15:55:08 +0800

    Update Airport Booking Management System.c

--- Airport Booking Management System.c
+++ Airport Booking Management System.c
@@ -26,7 +26,7 @@ void main()
 	do  
 	{ 
                 printf("\n\n\t\t ==============================================");
-		printf("\n\t\t     Agoo  Airport Booking Management System  v2.42 ");
+		printf("\n\t\t     Agoo  Airport Booking Management System  v2.50 ");
 		printf("\n\t\t =================================================");
 		printf("\n\n\n\t\t Please enter your choice from below (1-4):");
 		printf("\n\n\t\t 1. Make Reservation");
